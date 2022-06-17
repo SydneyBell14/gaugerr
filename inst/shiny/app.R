fe9c623b43b8c13a7dbc22b66450197eb3d2803d
Sydney Bell <bells2@carleton.edu>
2022-06-17 14:47:37 -0500

    update with Point Estimates

--- inst/shiny/app.R
+++ inst/shiny/app.R
@@ -6,14 +6,6 @@ library(stringr)
 library(tidyverse)
 library(readr)
 
-## means function
-
-
-
-
-
-
-
 ui <- navbarPage(title = "Shiny Gague R&R",
                  id = "gague",
                  tabPanel(
@@ -35,49 +27,6 @@ ui <- navbarPage(title = "Shiny Gague R&R",
                      ) # end sidebarLayout
                    ) # end fluidPage
                  ), # end Data Selection tab
-                 tabPanel(title = "Means",
-                   fluidPage(
-                     radioButtons(
-                       inputId = "click",
-                       label = "chose which means you want to see",
-                       choiceNames = c("Operator", "Part", "Operator and Part", "Total"),
-                       choiceValues = c("O", "P", "O,P", "T"),
-                       selected = "T"
-                     ), # end radioButtons
-                     tableOutput(
-                       outputId = "table"
-                       ) # end tableOutput
-                   ) # end fluidPage
-                 ), #end Means tab
-                 tabPanel(title = "Square Means Difference",
-                          fluidPage(
-                            radioButtons(
-                              inputId = "difference",
-                              label = "chose which squared difference you want to see",
-                              choiceNames = c("Operator", "Part", "Operator and Part"),
-                              choiceValues = c("O", "P", "O,P"),
-                              selected = "Part"
-                            ), # end radioButtons
-                            tableOutput(
-                              outputId = "tableSq"
-                              ) #end tableOutput
-                          ) #end fluidPage
-                        ), #end tabPanel
-                 tabPanel(title = "Sum of Squared Differences",
-                          fluidPage(
-                             radioButtons(
-                               inputId = "sumSquares",
-                               label = "chose a value you want estimated",
-                               choices = c("SSP", "SSO", "SSE", "SST", "SSPO")
-                             ),# end radioButtons
-                             tableOutput(
-                               outputId = "tableSumSq"
-                             ), # end tableOutput
-                             textOutput(
-                               outputId = "definition"
-                             ) # end textOutput
-                           ) # end fluidPage
-                 ), #end tabPanel
                  tabPanel(
                    title="Mean Sum of Squares",
                    fluidPage(
@@ -90,7 +39,25 @@ ui <- navbarPage(title = "Shiny Gague R&R",
                        outputId = "tableMeanSum"
                      ) #end tableOutput
                    ) #end fluidPage
-                 ) #end Mean Sum of Squares Panel
+                 ), #end Mean Sum of Squares Panel
+                 tabPanel(
+                   title = "Point Estimates",
+                   fluidPage(
+                     radioButtons(
+                       inputId = "est",
+                       label = "choose an estimate",
+                       choices = c("s2_repeat","s2_p","s2_o", "s2_po","s2_tot",
+                                   "s2_repro", "s2_gauge", "pg_ratio","gt_ratio",
+                                   "pr_ratio")
+                     ),# end radioButtons
+                     tableOutput(
+                       outputId = "tableEst"
+                     ) # end tableOutput
+                   ) # end fluidPage
+                 ), # end tabPanel
+                 tabPanel(
+                   title = "Confidence Intervals"
+                 )
 ) #end navbarPage
 
 server <- function(input, output){
@@ -108,128 +75,6 @@ server <- function(input, output){
   output$dataSelect <- renderText({
     paste("data selected", input$data)
   }) # end output$dataSelect
-
-  output$table <- renderTable({
-    if(input$click == "T"){
-      data_frame() %>%
-        summarize(ybar = mean(Y))
-    }else if(input$click == "O,P"){
-      data_frame() %>%
-        group_by(O,P) %>%
-        summarize(ybar = mean(Y))
-    }else{
-      data_frame() %>%
-        group_by(across(input$click)) %>%
-        summarize(ybar = mean(Y))
-    }
-  }) # end output$table
-
-  output$tableSq <- renderTable({
-    if(input$difference == "O,P"){
-      data_frame() %>%
-        group_by(O, P) %>%
-        mutate(ybar2 = mean(Y)) %>%
-        summarize(SSe = sum((ybar2-Y)^2))
-    } # end if statement (O)
-    else {
-      data_frame() %>%
-        group_by(across(input$difference)) %>%
-        mutate(ybar2 = mean(Y)) %>%
-        summarise(SSp = sum((ybar2-Y)^2))
-    }
-  }) # end output$tableSq
-
-  output$tableSumSq <- renderTable({
-    n <- nrow(data_frame())
-    p <- nrow(data_frame() %>%
-                group_by(P) %>%
-                summarize(ybar = mean(Y)))
-    o <- nrow(data_frame() %>%
-                group_by(O) %>%
-                summarize(ybar = mean(Y)))
-    r <- n/(o*p)
-
-    if(input$sumSquares == "SSP"){
-      data_frame() %>%
-        group_by(P)%>%
-        mutate(ybarI = mean(Y)) %>%
-        ungroup() %>%
-        mutate(ybar = mean(Y)) %>%
-        summarize(ssP1 = (ybarI-ybar)^2) %>%
-        distinct() %>%
-        summarize(SSP = sum(ssP1)* r * o)
-
-    }else if(input$sumSquares == "SSO"){
-      data_frame() %>%
-        group_by(O) %>%
-        mutate(ybarJ = mean(Y)) %>%
-        ungroup() %>%
-        mutate(ybar = mean(Y)) %>%
-        summarize(ssP1 = (ybarJ-ybar)^2) %>%
-        distinct() %>%
-        summarize(SSP = sum(ssP1)* r * p)
-
-    }else if(input$sumSquares == "SSE"){
-      data_frame()%>%
-        group_by(O, P) %>%
-        mutate(ybar2 = mean(Y)) %>%
-        summarize(SSe = sum((ybar2-Y)^2)) %>%
-        ungroup()%>%
-        summarize(SSE=sum(SSe))
-
-    }else if(input$sumSquares == "SST"){
-      data_frame()%>%
-        summarize(var = var(Y))%>%
-        summarize(SST = var*(n-1))
-
-    }else if(input$sumSquares == "SSPO"){
-      SSP <- data_frame() %>%
-        group_by(P)%>%
-        mutate(ybarI = mean(Y)) %>%
-        ungroup() %>%
-        mutate(ybar = mean(Y)) %>%
-        summarize(ssP1 = (ybarI-ybar)^2) %>%
-        distinct() %>%
-        summarize(SSP = sum(ssP1)* r * o)
-
-      SSO <- data_frame() %>%
-        group_by(O) %>%
-        mutate(ybarJ = mean(Y)) %>%
-        ungroup() %>%
-        mutate(ybar = mean(Y)) %>%
-        summarize(ssP1 = (ybarJ-ybar)^2) %>%
-        distinct() %>%
-        summarize(SSP = sum(ssP1)* r * p)
-
-      SSE <- data_frame()%>%
-        group_by(O, P) %>%
-        mutate(ybar2 = mean(Y)) %>%
-        summarize(SSe = sum((ybar2-Y)^2)) %>%
-        ungroup()%>%
-        summarize(SSE=sum(SSe))
-
-      SST <- data_frame()%>%
-        summarize(var = var(Y))%>%
-        summarize(SST = var*(n-1))
-
-      SSPO <- SST-sum(SSP, SSO, SSE)
-      paste(SSPO)
-    }
-  }) #end output$tableSumSq
-
-  output$definition <- renderText({
-    if(input$sumSquares == "SSP"){
-      paste("this tells us about the sum of square differences for all the parts")
-    }else if(input$sumSquares == "SSO"){
-      paste("this tells us about the sum of square differences for all the operators")
-    }else if(input$sumSquares == "SSE"){
-      paste("this tells us about the repeatability of the measurement")
-    }else if(input$sumSquares == "SSPO"){
-      paste("this tells us about the sum of square differences for the partop")
-    }else if(input$sumSquares == "SST"){
-      paste("this tells us about the total variation of the measurement")
-    }
-  }) #end output$definition
 
   #c("MSP", "MSO", "MSE", "MSPO")
   output$tableMeanSum <- renderTable({
@@ -288,6 +133,124 @@ server <- function(input, output){
     }
   })
 
+  output$tableEst <- renderTable({
+    n <- nrow(data_frame())
+    p <- nrow(data_frame() %>%
+                group_by(P) %>%
+                summarize(ybar = mean(Y)))
+    o <- nrow(data_frame() %>%
+                group_by(O) %>%
+                summarize(ybar = mean(Y)))
+    r <- n/(o*p)
+
+    MSE <- data_frame()%>%
+      group_by(O, P) %>%
+      mutate(ybar2 = mean(Y)) %>%
+      summarize(SSe = sum((ybar2-Y)^2)) %>%
+      ungroup()%>%
+      summarize(MSE=sum(SSe)/(p*o*(r-1)))
+
+    MSP <- data_frame() %>%
+      group_by(P)%>%
+      mutate(ybarI = mean(Y)) %>%
+      ungroup() %>%
+      mutate(ybar = mean(Y)) %>%
+      summarize(ssP1 = (ybarI-ybar)^2) %>%
+      distinct() %>%
+      summarize(MSP = (sum(ssP1)* r * o)/(p-1))
+
+    MSO <- data_frame() %>%
+      group_by(O) %>%
+      mutate(ybarJ = mean(Y)) %>%
+      ungroup() %>%
+      mutate(ybar = mean(Y)) %>%
+      summarize(ssP1 = (ybarJ-ybar)^2) %>%
+      distinct() %>%
+      summarize(MSO = (sum(ssP1)* r * p)/(o-1))
+
+    SSP <- data_frame() %>%
+      group_by(P)%>%
+      mutate(ybarI = mean(Y)) %>%
+      ungroup() %>%
+      mutate(ybar = mean(Y)) %>%
+      summarize(ssP1 = (ybarI-ybar)^2) %>%
+      distinct() %>%
+      summarize(SSP = sum(ssP1)* r * o)
+
+    SSO <- data_frame() %>%
+      group_by(O) %>%
+      mutate(ybarJ = mean(Y)) %>%
+      ungroup() %>%
+      mutate(ybar = mean(Y)) %>%
+      summarize(ssP1 = (ybarJ-ybar)^2) %>%
+      distinct() %>%
+      summarize(SSP = sum(ssP1)* r * p)
+
+    SSE <- data_frame()%>%
+      group_by(O, P) %>%
+      mutate(ybar2 = mean(Y)) %>%
+      summarize(SSe = sum((ybar2-Y)^2)) %>%
+      ungroup()%>%
+      summarize(SSE=sum(SSe))
+
+    SST <- data_frame()%>%
+      summarize(var = var(Y))%>%
+      summarize(SST = var*(n-1))
+
+    SSPO <- SST-sum(SSP, SSO, SSE)
+    MSPO <- SSPO/((p-1)*(o-1))
+
+    s2_p <-(MSP-MSPO)/(o*r)
+    s2_o <- (MSO-MSPO)/(p*r)
+    s2_po <- (MSPO-MSE)/r
+    s2_tot <- (p*MSP+o*MSO+(p*o-p-o)*MSPO+p*o*(r - 1)*MSE)/(p*o*r)
+    s2_repro <- (MSO+(p-1)*MSPO-p*MSE)/(p*r)
+    s2_gauge <- (MSO + (p-1)*MSPO + p*(r - 1)*MSE) / (p*r)
+    # c("s2_repeat","s2_p","s2_o", "s2_po","s2_tot",
+    # "s2_repro", "s2_gauge", "pg_ratio","gt_ratio",
+    # "pr_ratio")
+
+    if (input$est == "s2_repeat"){
+        paste(MSE)
+    }else if (input$est == "s2_p"){
+        if (s2_p > 0){
+          paste(s2_p)
+        }else{
+          paste(0)
+        }
+    }else if (input$est == "s2_o"){
+        if(s2_o > 0){
+          paste(s2_o)
+        }else{
+          paste(0)
+        }
+    }else if (input$est == "s2_po"){
+      if(s2_po > 0){
+        paste(s2_po)
+      }else{
+        paste(0)
+      }
+    }else if (input$est == "s2_tot"){
+      paste(s2_tot)
+    }else if (input$est == "s2_repro"){
+      if(s2_repro > 0){
+        paste(s2_repro)
+      }else{
+        paste(0)
+      }
+    }else if(input$est == "s2_gauge"){
+      paste(s2_gauge)
+    }else if(input$est == "pg_ratio"){
+      pg_ratio <- s2_p / s2_gauge
+      paste(pg_ratio)
+    }else if(input$est == "gt_ratio"){
+      gt_ratio <- s2_gauge/s2_tot
+      paste(gt_ratio)
+    }else if(input$est == "pr_ratio"){
+      pr_ratio <- s2_p/MSE
+      paste(pr_ratio)
+    }
+  })
 }
 
 shinyApp(ui, server)

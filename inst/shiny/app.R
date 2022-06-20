32de4c58f0fe54ae0d9c14746b135e0f04d6a617
Sydney Bell <bells2@carleton.edu>
2022-06-20 18:21:41 -0500

    update with CIs 6/20

--- inst/shiny/app.R
+++ inst/shiny/app.R
@@ -56,8 +56,28 @@ ui <- navbarPage(title = "Shiny Gague R&R",
                    ) # end fluidPage
                  ), # end tabPanel
                  tabPanel(
-                   title = "Confidence Intervals"
-                 )
+                   title = "Confidence Intervals",
+                   fluidPage(
+                    radioButtons(
+                      inputId = "conf",
+                      label = "choose an estimator you want the confidence interval for",
+                      choices = c("s2_repeat","s2_p","s2_o", "s2_po","s2_tot",
+                                 "s2_repro", "s2_gauge", "pg_ratio","gt_ratio",
+                                 "pr_ratio")
+                    ),# end radioButtons
+                    sliderInput(
+                      inputId = "alpha",
+                      label = "chose a value for alpha",
+                      min = 0,
+                      max = 1,
+                      value = 0.90,
+                      step = 0.05
+                    ),# end sliderInput
+                    tableOutput(
+                      outputId = "tableConf"
+                      ) # end tableOutput
+                   ) # end fluidPage
+                 ) # end tabPanel
 ) #end navbarPage
 
 server <- function(input, output){
@@ -72,13 +92,65 @@ server <- function(input, output){
     }# end else if statement
   }) # end data_frame
 
+  MSE <- reactive({data_frame()%>%
+      group_by(O, P) %>%
+      mutate(ybar2 = mean(Y)) %>%
+      summarize(SSe = sum((ybar2-Y)^2)) %>%
+      ungroup()%>%
+      summarize(MSE=sum(SSe)/(p*o*(r-1)))}) #end MSE
+
+  MSP <- reactive({data_frame() %>%
+      group_by(P) %>%
+      mutate(ybarI = mean(Y)) %>%
+      ungroup() %>%
+      mutate(ybar = mean(Y)) %>%
+      summarize(ssP1 = (ybarI-ybar)^2) %>%
+      distinct() %>%
+      summarize(MSP = (sum(ssP1)* r * o)/(p-1))}) #end MSP
+
+  MSO <- reactive({data_frame() %>%
+      group_by(O) %>%
+      mutate(ybarJ = mean(Y)) %>%
+      ungroup() %>%
+      mutate(ybar = mean(Y)) %>%
+      summarize(ssP1 = (ybarJ-ybar)^2) %>%
+      distinct() %>%
+      summarize(MSO = (sum(ssP1)* r * p)/(o-1))}) #end MSO
+
+  SSP <- reactive({data_frame() %>%
+      group_by(P)%>%
+      mutate(ybarI = mean(Y)) %>%
+      ungroup() %>%
+      mutate(ybar = mean(Y)) %>%
+      summarize(ssP1 = (ybarI-ybar)^2) %>%
+      distinct() %>%
+      summarize(SSP = sum(ssP1)* r * o)}) #end SSP
+
+  SSO <- reactive({data_frame() %>%
+      group_by(O) %>%
+      mutate(ybarJ = mean(Y)) %>%
+      ungroup() %>%
+      mutate(ybar = mean(Y)) %>%
+      summarize(ssP1 = (ybarJ-ybar)^2) %>%
+      distinct() %>%
+      summarize(SSO = sum(ssP1)* r * p)}) #end SSO
+  SSE <- reactive({data_frame()%>%
+      group_by(O, P) %>%
+      mutate(ybar2 = mean(Y)) %>%
+      summarize(SSe = sum((ybar2-Y)^2)) %>%
+      ungroup()%>%
+      summarize(SSE=sum(SSe))})
+  SST<-reactive({data_frame()%>%
+      summarize(var = var(Y))%>%
+      summarize(SST = var*(n-1))})
+
   output$dataSelect <- renderText({
     paste("data selected", input$data)
   }) # end output$dataSelect
 
   #c("MSP", "MSO", "MSE", "MSPO")
   output$tableMeanSum <- renderTable({
-    n <- nrow(data_frame())
+    n <-  nrow(data_frame())
     p <- nrow(data_frame() %>%
                 group_by(P) %>%
                 summarize(ybar = mean(Y)))
@@ -87,46 +159,14 @@ server <- function(input, output){
                 summarize(ybar = mean(Y)))
     r <- n/(o*p)
 
-    SSP <- data_frame() %>%
-      group_by(P)%>%
-      mutate(ybarI = mean(Y)) %>%
-      ungroup() %>%
-      mutate(ybar = mean(Y)) %>%
-      summarize(ssP1 = (ybarI-ybar)^2) %>%
-      distinct() %>%
-      summarize(SSP = sum(ssP1)* r * o)
-
-    SSO <- data_frame() %>%
-      group_by(O) %>%
-      mutate(ybarJ = mean(Y)) %>%
-      ungroup() %>%
-      mutate(ybar = mean(Y)) %>%
-      summarize(ssP1 = (ybarJ-ybar)^2) %>%
-      distinct() %>%
-      summarize(SSP = sum(ssP1)* r * p)
-
-    SSE <- data_frame()%>%
-      group_by(O, P) %>%
-      mutate(ybar2 = mean(Y)) %>%
-      summarize(SSe = sum((ybar2-Y)^2)) %>%
-      ungroup()%>%
-      summarize(SSE=sum(SSe))
-
-    SST <- data_frame()%>%
-      summarize(var = var(Y))%>%
-      summarize(SST = var*(n-1))
-
-    SSPO <- SST-sum(SSP, SSO, SSE)
+    SSPO <- SST()-sum(SSP(), SSO(), SSE())
 
     if (input$meanSums == "MSP"){
-      MSP <- SSP/(p-1)
-      paste(MSP)
+      paste(MSP())
     }else if(input$meanSums == "MSO"){
-      MSO <- SSO/(o-1)
-      paste(MSO)
+      paste(MSO())
     }else if(input$meanSums == "MSE"){
-      MSE <- SSE/(p*o*(r-1))
-      paste(MSE)
+      paste(MSE())
     }else if(input$meanSums == "MSPO"){
       MSPO <- SSPO/((p-1)*(o-1))
       paste(MSPO)
@@ -142,73 +182,19 @@ server <- function(input, output){
                 group_by(O) %>%
                 summarize(ybar = mean(Y)))
     r <- n/(o*p)
+    MSE <- MSE()
+    MSP <- MSP()
+    MSO <- MSO()
 
-    MSE <- data_frame()%>%
-      group_by(O, P) %>%
-      mutate(ybar2 = mean(Y)) %>%
-      summarize(SSe = sum((ybar2-Y)^2)) %>%
-      ungroup()%>%
-      summarize(MSE=sum(SSe)/(p*o*(r-1)))
-
-    MSP <- data_frame() %>%
-      group_by(P)%>%
-      mutate(ybarI = mean(Y)) %>%
-      ungroup() %>%
-      mutate(ybar = mean(Y)) %>%
-      summarize(ssP1 = (ybarI-ybar)^2) %>%
-      distinct() %>%
-      summarize(MSP = (sum(ssP1)* r * o)/(p-1))
-
-    MSO <- data_frame() %>%
-      group_by(O) %>%
-      mutate(ybarJ = mean(Y)) %>%
-      ungroup() %>%
-      mutate(ybar = mean(Y)) %>%
-      summarize(ssP1 = (ybarJ-ybar)^2) %>%
-      distinct() %>%
-      summarize(MSO = (sum(ssP1)* r * p)/(o-1))
-
-    SSP <- data_frame() %>%
-      group_by(P)%>%
-      mutate(ybarI = mean(Y)) %>%
-      ungroup() %>%
-      mutate(ybar = mean(Y)) %>%
-      summarize(ssP1 = (ybarI-ybar)^2) %>%
-      distinct() %>%
-      summarize(SSP = sum(ssP1)* r * o)
-
-    SSO <- data_frame() %>%
-      group_by(O) %>%
-      mutate(ybarJ = mean(Y)) %>%
-      ungroup() %>%
-      mutate(ybar = mean(Y)) %>%
-      summarize(ssP1 = (ybarJ-ybar)^2) %>%
-      distinct() %>%
-      summarize(SSP = sum(ssP1)* r * p)
-
-    SSE <- data_frame()%>%
-      group_by(O, P) %>%
-      mutate(ybar2 = mean(Y)) %>%
-      summarize(SSe = sum((ybar2-Y)^2)) %>%
-      ungroup()%>%
-      summarize(SSE=sum(SSe))
-
-    SST <- data_frame()%>%
-      summarize(var = var(Y))%>%
-      summarize(SST = var*(n-1))
-
-    SSPO <- SST-sum(SSP, SSO, SSE)
+    SSPO <- SST()-sum(SSP(), SSO(), SSE())
     MSPO <- SSPO/((p-1)*(o-1))
 
     s2_p <-(MSP-MSPO)/(o*r)
     s2_o <- (MSO-MSPO)/(p*r)
     s2_po <- (MSPO-MSE)/r
-    s2_tot <- (p*MSP+o*MSO+(p*o-p-o)*MSPO+p*o*(r - 1)*MSE)/(p*o*r)
+    s2_tot <- (p*MSP+o*MSO+(p*o-p-o)*MSPO+p*o*(r-1)*MSE)/(p*o*r)
     s2_repro <- (MSO+(p-1)*MSPO-p*MSE)/(p*r)
     s2_gauge <- (MSO + (p-1)*MSPO + p*(r - 1)*MSE) / (p*r)
-    # c("s2_repeat","s2_p","s2_o", "s2_po","s2_tot",
-    # "s2_repro", "s2_gauge", "pg_ratio","gt_ratio",
-    # "pr_ratio")
 
     if (input$est == "s2_repeat"){
         paste(MSE)
@@ -249,6 +235,133 @@ server <- function(input, output){
     }else if(input$est == "pr_ratio"){
       pr_ratio <- s2_p/MSE
       paste(pr_ratio)
+    }
+  })
+  output$tableConf <- renderTable({
+    n <- nrow(data_frame())
+    p <- nrow(data_frame() %>%
+                group_by(P) %>%
+                summarize(ybar = mean(Y)))
+    o <- nrow(data_frame() %>%
+                group_by(O) %>%
+                summarize(ybar = mean(Y)))
+    r <- n/(o*p)
+    MSE <- MSE()
+    MSP <- MSP()
+    MSO <- MSO()
+
+    SSPO <- SST()-sum(SSP(), SSO(), SSE())
+    MSPO <- SSPO/((p-1)*(o-1))
+
+    s2_p <-(MSP-MSPO)/(o*r)
+    s2_o <- (MSO-MSPO)/(p*r)
+    s2_po <- (MSPO-MSE)/r
+    s2_tot <- (p*MSP+o*MSO+(p*o-p-o)*MSPO+p*o*(r-1)*MSE)/(p*o*r)
+    s2_repro <- (MSO+(p-1)*MSPO-p*MSE)/(p*r)
+    s2_gauge <- (MSO + (p-1)*MSPO + p*(r - 1)*MSE) / (p*r)
+    pg_ratio <- s2_p / s2_gauge
+    gt_ratio <- s2_gauge/s2_tot
+    pr_ratio <- s2_p/MSE
+
+    # alpha value
+    alpha <- input$alpha
+
+    # coefficients
+    G1 <- 1 - qf(alpha/2, Inf, p - 1)
+    G2 <- 1 - qf(alpha/2, Inf, o - 1)
+    G3 <- 1 - qf(alpha/2, Inf, (p-1) * (o-1))
+    G4 <- 1 - qf(alpha/2, Inf, p * o * (r - 1))
+    H1 <- qf(1 - alpha/2, Inf, p - 1) - 1
+    H2 <- qf(1 - alpha/2, Inf, o - 1) - 1
+    H3 <- qf(1 - alpha/2, Inf, (p-1) * (o-1)) - 1
+    H4 <- qf(1 - alpha/2, Inf, p * o * (r - 1)) - 1
+    F1 <- qf(1 - alpha/2, p - 1, (p - 1) * (o - 1))
+    F2 <- qf(alpha/2, p - 1, (p - 1) * (o - 1))
+    F3 <- qf(1 - alpha/2, p - 1, o - 1)
+    F4 <- qf(alpha/2, p - 1, o - 1)
+    F5 <- qf(1 - alpha/2, o - 1, (p - 1) *(o - 1))
+    F6 <- qf(alpha/2, o - 1, (p - 1) *(o - 1))
+    F7 <- qf(1 - alpha/2, (p - 1) * (o - 1), p * o * (r - 1))
+    F8 <- qf(alpha/2, (p - 1) * (o - 1), p * o * (r - 1))
+    F9 <- qf(1 - alpha/2, p - 1, p * o * (r - 1))
+    F10 <- qf(alpha/2, p - 1, p * o * (r - 1))
+    F11 <- qf(1 - alpha/2, o - 1, p * o * (r - 1))
+    F12 <- qf(alpha/2, o - 1, p * o * (r - 1))
+    G13 <- ((F1 - 1)^2 - G1^2 * F1^2 - H3^2) / F1
+    G23 <- ((F5 - 1)^2 - G2^2 * F5^2 - H3^2) / F5
+    G23.star <- (1 - 1/qf(1-alpha/2, p * (o - 1), Inf))^2 * (p^2 / (p - 1)) -
+      G2^2 / (p-1) - G3^2 * (p-1)
+    G24 <- ((1 - F11)^2 - G2^2 * F11^2 - H4^2) / F11
+    G34 <- ((1 - F7)^2 - G3^2 * F7^2 - H4^2) / F7
+    H13 <- ((1 - F2)^2 - H1^2 * F2^2 - G3^2) / F2
+    H23 <- ((1 - F6)^2 - H2^2 * F6^2 - G3^2) / F6
+    H24 <- ((1 - F12)^2 - H2^2 * F12^2 - G4^2) / F12
+    H34 <- ((1 - F8)^2 - H3^2 * F8^2 - G4^2) / F8
+
+    if(input$conf == "s2_repeat"){
+
+      paste("interval")
+    }else if(input$conf =="s2_p"){
+      v_lp <- G1^2 * MSP^2 + H3^2 * MSPO^2 + G13 * MSP * MSPO
+      v_up <- H1^2 * MSP^2 + G3^2 * MSPO^2 + H13 * MSP * MSPO
+      parts.lower <- s2_p - sqrt(v_lp) / (o * r)
+      parts.upper <- s2_p + sqrt(v_up) / (o * r)
+      paste("lower:", parts.lower, " upper:",parts.upper)
+    }else if(input$conf =="s2_o"){
+      v_lo <- G2^2 * MSO^2 + H3^2 * MSPO^2 + G23 * MSO * MSPO
+      v_uo <- H2^2 * MSO^2 + G3^2 * MSPO^2 + H23 * MSO * MSPO
+      oper.lower <- s2_o - sqrt(v_lo) / (p * r)
+      oper.upper <- s2_o + sqrt(v_uo) / (p * r)
+      paste("lower:", oper.lower, " upper:", oper.upper)
+    }else if(input$conf =="s2_po"){
+      v_lpo <- G3^2 * MSPO^2 + H4^2 * MSE^2 + G34 * MSPO * MSE
+      v_upo <- H3^2 * MSPO^2 + G4^2 * MSE^2 + H34 * MSPO * MSE
+      po.lower <- s2_po - sqrt(v_lpo) / r
+      po.upper <- s2_po + sqrt(v_upo) / r
+      paste("lower:", po.lower, " upper:", po.upper)
+    }else if(input$conf =="s2_tot"){
+      v_lt <- G1^2 * p^2 * MSP^2 +
+        G2^2 * o^2 * MSO^2 +
+        G3^2 * (p * o - p - o)^2 * MSPO^2 +
+        G4^2 * (p * o)^2 * (r - 1)^2 * MSE^2
+      v_ut <- H1^2 * p^2 * MSP^2 +
+        H2^2 * o^2 * MSO^2 +
+        H3^2 * (p * o - p - o)^2 * MSPO^2 +
+        H4^2 * (p * o)^2 * (r - 1)^2 * MSE^2
+      total.lower <- s2_tot - sqrt(v_lt) / (p * o * r)
+      total.upper <- s2_tot + sqrt(v_ut) / (p * o * r)
+      paste("lower:", total.lower, " upper:", total.upper)
+    }else if(input$conf =="s2_repro"){
+      v_lrepro <- (G2^2 * MSO^2 +
+                     G3^2 * (p - 1)^2 * MSPO^2 +
+                     H4^2 * p^2 * MSE^2 +
+                     G24 * p * MSO * MSE +
+                     G34 * (p - 1) * p * MSPO * MSE +
+                     G23.star * (p - 1) * MSO * MSPO) / (p * r)^2
+      v_urepo <- (H2^2 * MSO^2 +
+                    H3^2 * (p - 1)^2 * MSPO^2 +
+                    G4^2 * p^2 * MSE^2 +
+                    H24 * p * MSO * MSE +
+                    H34 * (p - 1) * p * MSPO * MSE) / (p * r)^2
+      repro.lower <- s2_repro - sqrt(v_lrepro)
+      repro.upper <- s2_repro + sqrt(v_urepo)
+      paste("lower:", repro.lower, " upper:", repro.upper)
+    }else if(input$conf =="s2_gauge"){
+      v_lm <- G2^2 * MSO^2 + G3^2 * (p - 1)^2 * MSPO^2 + G4^2 * p^2 * (r - 1)^2 * MSE^2
+      v_um <- H2^2 * MSO^2 + H3^2 * (p - 1)^2 * MSPO^2 + H4^2 * p^2 * (r - 1)^2 * MSE^2
+      gauge.lower <- s2_gauge - sqrt(v_lm) / (p * r)
+      gauge.upper <- s2_gauge + sqrt(v_um) / (p * r)
+      paste("lower:", gauge.lower, " upper:", gauge.upper)
+    }else if(input$conf =="pg_ratio"){
+      pg_ratio_lower <- (p * (1 - G1) * (MSP - F1 * MSPO)) /
+        (p * o * (r - 1) * MSE + o * (1 - G1) * F3 * MSO + o * (p - 1) * MSPO)
+      pg_ratio_upper <- (p * (1 + H1) * (MSP - F2 * MSPO)) /
+        (p * o * (r - 1) * MSE + o * (1 + H1) * F4 * MSO + o * (p - 1) * MSPO)
+      paste("lower:", pg_ratio_lower, " upper:", pg_ratio_upper)
+    }else if(input$conf =="gt_ratio"){
+      paste("interval")
+    }else if(input$conf =="pr_ratio"){
+      paste("interval")
     }
   })
 }
